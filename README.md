c9876f4a95071edfb90f959c8a0e32aa6d974983
Nikhil Lingutla <nikhil.lingutla@gmail.com>
2012-12-20 13:35:54 -0800

    [MessageBoard] Modify README file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-A simple Forum like MessageBoard 
+# A simple Forum like MessageBoard #
+
+## Functionalities ## 
+  
+1. A user can Sign In using Twitter (used omniauth-twitter).
+2. A user can create a new post.
+3. A user can edit an existing post created by him. A user cannot edit/delete another users posts.
+4. A user can see posts made by other users.
+5. A user can Signout.
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
     auth              = request.env["omniauth.auth"]
     user              = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_from_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "signed in"
+    redirect_to root_url
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "signed Out"
+    redirect_to root_url
   end
 
 end
