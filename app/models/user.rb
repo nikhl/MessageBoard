37537c2c679b9dfc9cdc76b31244578b2cde2cf8
Nikhil Lingutla <nikhil.lingutla@gmail.com>
2012-12-24 11:33:13 -0800

    [MessageBoard] Modify creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
 
   def self.create_from_omniauth(auth)
-    if auth["provider"] == "twitter"
-      create! do |user|
-        user.provider = auth["provider"]
-        user.uid      = auth["uid"]
-        user.name     = auth["info"]["name"]
-      end
-    else
-      where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-        user.provider         = auth.provider
-        user.uid              = auth.uid
-        user.name             = auth.info.name
+    create! do |user|
+      user.provider = auth.provider
+      user.uid      = auth.uid
+      user.name     = auth.info.name
+      if auth.provider == "facebook"
         user.oauth_token      = auth.credentials.token
         user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-        user.save!
       end
     end
   end
