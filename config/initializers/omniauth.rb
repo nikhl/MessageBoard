8dcf4cc7f35fb43afdfbf7b5d765cadf230077fb
Nikhil Lingutla <nikhil.lingutla@gmail.com>
2012-12-29 22:01:10 -0800

    [MessageBoard] Modify to save users email via various authentications

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # Twitter doesnot provide email through OmniAuth, hence no need to scope email
   provider :twitter,  APP_CONFIG['twitter_id'], APP_CONFIG['twitter_secret']
-  provider :facebook, APP_CONFIG['facebook_id'], APP_CONFIG['facebook_secret']
-  provider :github, APP_CONFIG['github_id'], APP_CONFIG['github_secret']
-  provider :foursquare, APP_CONFIG['foursquare_id'], APP_CONFIG['foursquare_secret']
-  provider :linkedin, APP_CONFIG['linkedin_id'], APP_CONFIG['linkedin_secret']
-  provider :google, APP_CONFIG['google_id'], APP_CONFIG['google_secret']
+  provider :facebook, APP_CONFIG['facebook_id'], APP_CONFIG['facebook_secret'], :scope => 'email'
+  provider :github, APP_CONFIG['github_id'], APP_CONFIG['github_secret'], :scope => 'email'
+  provider :foursquare, APP_CONFIG['foursquare_id'], APP_CONFIG['foursquare_secret'], :scope => 'email'
+  provider :linkedin, APP_CONFIG['linkedin_id'], APP_CONFIG['linkedin_secret'], :scope => 'r_emailaddress'
+  provider :google, APP_CONFIG['google_id'], APP_CONFIG['google_secret'], :scope => 'email'
   provider :tumblr, APP_CONFIG['tumblr_id'], APP_CONFIG['tumblr_secret']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121229064028) do
+ActiveRecord::Schema.define(:version => 20121230033041) do
 
   create_table "posts", :force => true do |t|
     t.string   "title",      :null => false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20121229064028) do
     t.datetime "updated_at",       :null => false
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "email"
   end
 
 end
