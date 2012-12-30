55aed18dba681f9db0cdc4a361441910a67cf437
Nikhil Lingutla <nikhil.lingutla@gmail.com>
2012-12-29 22:33:44 -0800

    [MessageBoard] Change incorrect google authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, APP_CONFIG['github_id'], APP_CONFIG['github_secret'], :scope => 'email'
   provider :foursquare, APP_CONFIG['foursquare_id'], APP_CONFIG['foursquare_secret'], :scope => 'email'
   provider :linkedin, APP_CONFIG['linkedin_id'], APP_CONFIG['linkedin_secret'], :scope => 'r_emailaddress'
-  provider :google, APP_CONFIG['google_id'], APP_CONFIG['google_secret'], :scope => 'email'
+  provider :google, APP_CONFIG['google_id'], APP_CONFIG['google_secret'], :scope => 'r_emailaddress'
   provider :tumblr, APP_CONFIG['tumblr_id'], APP_CONFIG['tumblr_secret']
 end
